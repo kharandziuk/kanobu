7a88d7e797f1a14355510b5f0d0105a91c0b5208
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-09-23 14:56:28 +0300

    changes to setup

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name='my_project',
     version='0.1.0',
     packages=['project'],
+    install_requires=[
+      'Django',
+      'pathlib',
+    ],
+    entry_points={
+      'console_scripts': [
+        'my_package = project.wsgi:application'
+      ]
+    },
 )
